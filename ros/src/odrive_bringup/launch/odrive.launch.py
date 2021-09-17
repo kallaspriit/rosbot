676bbb93eaea86f41387c76767b28f49fa8aae4e
Priit Kallas <kallaspriit@gmail.com>
2021-09-17 18:52:40 +0300

    Enabled right wheel joint by default

--- ros/src/odrive_bringup/launch/odrive.launch.py
+++ ros/src/odrive_bringup/launch/odrive.launch.py
@@ -33,7 +33,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "enable_right_wheel_joint",
-            default_value="false",
+            default_value="true",
         )
     )
 
