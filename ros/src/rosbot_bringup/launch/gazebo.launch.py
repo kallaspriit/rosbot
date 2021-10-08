bda13a8df665a214b4486d4b07d3565716e037a0
Priit Kallas <kallaspriit@gmail.com>
2021-10-08 15:10:51 +0300

    Configured robot inertia origin

--- ros/src/rosbot_bringup/launch/gazebo.launch.py
+++ ros/src/rosbot_bringup/launch/gazebo.launch.py
@@ -220,7 +220,7 @@ def generate_launch_description():
                 'rosbot_xbox_usb.config.yaml'
             ),
         ],
-        remappings={('/cmd_vel', 'diff_drive_controller/cmd_vel_unstamped')},
+        # remappings={('/cmd_vel', 'diff_drive_controller/cmd_vel_unstamped')},
         condition=IfCondition(LaunchConfiguration('launch_teleop')),
     )
 
