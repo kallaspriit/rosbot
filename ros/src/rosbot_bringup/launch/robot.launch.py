d5e532f5f6876806f5903e00d38db2a492eff7a9
Priit Kallas <priit@stagnationlab.com>
2021-11-11 19:29:59 +0200

    Added imu and robot localization nodes to main robot launch

--- ros/src/rosbot_bringup/launch/robot.launch.py
+++ ros/src/rosbot_bringup/launch/robot.launch.py
@@ -121,24 +121,20 @@ def generate_launch_description():
         ),
 
         # imu (used by robot_localization to make odometry more precise)
-        # Node(
-        #     package="bno055",
-        #     executable="bno055",
-        #     # name="imu",
-        #     # arguments=["--params-file", bno055_config],
-        #     parameters=[bno055_config],
-        # ),
+        Node(
+            package="bno055",
+            executable="bno055",
+            parameters=[bno055_config],
+        ),
 
         # fuses imu and odometry to produce more precise filtered odometry
-        # Node(
-        #     package="robot_localization",
-        #     executable="ekf_node",
-        #     name="robot_localization_node",
-        #     output="screen",
-        #     parameters=[
-        #         robot_localization_config,
-        #     ]
-        # ),
+        Node(
+            package="robot_localization",
+            executable="ekf_node",
+            name="robot_localization_node",
+            output="screen",
+            parameters=[robot_localization_config]
+        ),
 
         # joystick, used by onboard teleop
         Node(
