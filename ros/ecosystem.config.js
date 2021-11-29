34ee83e9a119469f6da59db9a7a3456dc5940316
Priit Kallas <priit@stagnationlab.com>
2021-11-29 19:02:59 +0200

    Added gazebo simulation to pm2 ecosystem file

--- ros/ecosystem.config.js
+++ ros/ecosystem.config.js
@@ -4,10 +4,6 @@ module.exports = {
     script: "launch_rviz.bash",
     interpreter: "bash"
   },{
-    name: "slam",
-    script: "launch_slam.bash",
-    interpreter: "bash"
-  },{
     name: "localization",
     script: "launch_localization.bash",
     interpreter: "bash"
@@ -16,12 +12,20 @@ module.exports = {
     script: "launch_navigation.bash",
     interpreter: "bash"
   },{
+    name: "slam",
+    script: "launch_slam.bash",
+    interpreter: "bash"
+  },{
     name: "teleop",
     script: "launch_teleop.bash",
     interpreter: "bash"
   }, {
     name: "robot",
     script: "launch_robot.bash",
+    interpreter: "bash"
+  }, {
+    name: "gazebo",
+    script: "launch_gazebo.bash",
     interpreter: "bash"
   }],
 };
